e6de3a60be74d6c9cba5604df590f85d99d8eab8
mike-gallego-2 <mgallego.business@gmail.com>
2022-05-01 09:49:10 -0400

    :white_check_mark: add unit tests

--- newer_flutter/test/widget_test.dart
+++ newer_flutter/test/widget_test.dart
@@ -5,26 +5,63 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:interview/models/book.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-import 'package:newer_flutter/main.dart';
+void sqfliteTestInit() {
+  // Initialize ffi implementation
+  sqfliteFfiInit();
+  // Set global factory
+  databaseFactory = databaseFactoryFfi;
+}
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+void main() async {
+  sqfliteTestInit();
+  var db = await openDatabase(inMemoryDatabasePath);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  test('check if db exists', () {
+    expect(db, isNotNull);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  test('create from Book class', () async {
+    await db.execute('''
+      CREATE TABLE book (
+        ID INTEGER,
+        Title TEXT,
+        Author TEXT,
+        CoverImage TEXT
+      )
+    ''');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await db.insert('book',
+        Book.fromMap({'ID': 1, 'Title': 'Harry Potter', 'Author': 'J.K. Rowling', 'CoverImage': 'Image'}).toMap());
+
+    var result = await db.rawQuery('SELECT * FROM book');
+    expect(result.length, 1);
+  });
+
+  test('update from Book class', () async {
+    await db.update(
+        'book',
+        Book.fromMap({
+          'ID': 1,
+          'Title': 'Harry Potter and the Goblet of Fire',
+          'Author': 'J.K. Rowling',
+          'CoverImage': 'Image'
+        }).toMap(),
+        where: 'ID = ?',
+        whereArgs: [1]);
+
+    var result = await db.rawQuery('SELECT * FROM book');
+    expect(result[0]['Title'], 'Harry Potter and the Goblet of Fire');
+  });
+
+  test('delete from Book class', () async {
+    await db.delete('book', where: 'ID = ?', whereArgs: [1]);
+
+    var result = await db.rawQuery('SELECT * FROM book');
+    expect(result.length, 0);
   });
 }
